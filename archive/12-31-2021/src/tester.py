ce91b08295802359fb5369a91263daa57e1588ee
Justin Holmes <jholmes802@gmail.com>
2022-01-14 22:55:40 -0500

    Delete archive directory

--- archive/12-31-2021/src/tester.py
+++
@@ -1,9 +0,0 @@
-import re
-
-string1 = "/item/217-6515/"
-string2 = string1 + "edit"
-
-test = re.search("^/item/.*/$", string1)
-print(test == True)
-test = re.search("^/item/.*/$", string2)
-print(test)
